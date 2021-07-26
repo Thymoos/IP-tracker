d51cfb13e76af310e29891465d5c7c5c3a6864c3
Konrad Izdebski <konradizdebski1011@gmail.com>
2021-07-26 10:44:47 +0200

    create main components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import Map from './components/Map';
+import Display from './components/Display';
 
 function App() {
   return (
     <div className="App">
-     heyo
+     <header>
+       <h1>IP Address Tracker</h1>
+       <form action="">
+         <input type="text" placeholder="Search for any IP address or domain" />
+         <button type="submit">&gt;</button>
+       </form>
+     </header>
+      <Display/>
+     <Map/>
     </div>
   );
 }
---
+++ src/components/Display.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Display.css';
+
+const Display = () => {
+    return ( 
+        <>
+        Display
+        </>
+     );
+}
+ 
+export default Display;
