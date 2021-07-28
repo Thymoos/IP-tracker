c80efc273c350d01fcffe3bc4cfeb7453749cf89
Konrad Izdebski <konradizdebski1011@gmail.com>
2021-07-28 18:14:56 +0200

    Add location marker

--- src/components/Icon.js
+++ src/components/Icon.js
@@ -1,8 +1,8 @@
 import L from 'leaflet';
 
 const icon =new L.Icon({
-    iconUrl: require('./marker-32.png'),
-    iconRetinaUrl: require('./marker-32.png'),
+    iconUrl: require('./icons8-marker-32.png'),
+    iconRetinaUrl: require('./icons8-marker-32.png'),
     iconSize: 32,
 });
 
--- src/components/Tracker.js
+++ src/components/Tracker.js
@@ -1,15 +1,17 @@
 import React from "react";
 import { MapContainer, TileLayer, Marker, Popup} from "react-leaflet";
-import icon from './Icon';
+import icon from 'leaflet/dist/images/marker-icon.png';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
 import L from 'leaflet';
-import 'leaflet/dist/leaflet.css';
 
 const position = [49.688919, 19.200649];
 
-L.Icon.Default.mergeOptions({
-    iconRetinaUrl: require('./marker-32.png'),
-    iconUrl: require('./marker-32.png')
+let DefaultIcon = L.icon({
+  iconUrl: icon,
+  shadowUrl: iconShadow
 });
+
+L.Marker.prototype.options.icon = DefaultIcon;
 
 export default function Map() {
 
@@ -19,15 +21,17 @@ export default function Map() {
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={position} icon={icon}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
+      <Marker position={position}>
+        <Popup><span>
+          Placeholder <br />
+          Here might be something <br />
+          In the future
+          </span>
         </Popup>
       </Marker>
     </MapContainer>
   );
 }
-
 
 
 
