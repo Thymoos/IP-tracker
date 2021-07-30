a44fbf9540fbada6b384d13df905198af4390e83
Konrad Izdebski <konradizdebski1011@gmail.com>
2021-07-30 13:23:29 +0200

    Add search function

--- src/App.js
+++ src/App.js
@@ -8,23 +8,52 @@ const defaultCords = {
   "longitude": 19.200649
 }
 
+const defaultData = {
+  "ip": 111,
+  "location": {
+    "city": "Żywiec",
+    "timezone": 420
+  },
+  "isp": "Monke"
+}
+
 function App() {
   const [cords, setCords] = useState(defaultCords);
-  const [data, setData] = useState();
+  const [data, setData] = useState(defaultData);
+  const [search, setSearch] = useState("");
+  const [click, setClick] = useState(false);
 
-  useEffect(()=>{
-    fetch('https://geo.ipify.org/api/v1?apiKey=at_J9T6UXAUk2lTqjQD5LbX2XnlndjHg&ipAddress&domain')
-      .then((response) => response.json())
-      .then((data) => setData(data));
-  },[]);
+  const url = `https://geo.ipify.org/api/v1?apiKey=at_J9T6UXAUk2lTqjQD5LbX2XnlndjHg&ipAddress=${search}&domain=${search}`;
+
+    useEffect(() => {
+      const fetchData = async () => {
+        try {
+          const response = await fetch(url);
+          const json = await response.json();
+          setData(json);
+          const coords = await [json.location.lat, json.location.lng];
+          setCords({
+            "latitude": coords[0],
+            "longitude": coords[1]
+          })
+        } catch (error) {
+          console.log("error", error);
+        }
+      };
+  
+      fetchData();
+  }, [click]);
+
+  const handleChange = (e) =>{
+    const input = e.target.value;
+    
+    setSearch(input);
+  }
+
 
   const handleClick = (e) =>{
     e.preventDefault();
-
-    setCords({
-      "latitude": data.location.lat,
-      "longitude": data.location.lng
-    });
+    setClick(!click)
   }
 
   return (
@@ -32,11 +61,11 @@ function App() {
      <header>
        <h1>IP Address Tracker</h1>
        <form action="">
-         <input type="text" spellCheck="false" placeholder="Search for any IP address or domain" />
+         <input onChange={handleChange} type="text" spellCheck="false" placeholder="Search for any IP address or domain" />
          <button type="submit" onClick={handleClick}>&gt;</button>
        </form>
      </header>
-      <Display/>
+      <Display ip={data.ip} location={data.location.city} timezone={data.location.timezone} isp={data.isp}/>
      <Tracker cords={cords}/>
     </div>
   );
--- src/components/Display.js
+++ src/components/Display.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './Display.css';
 
-const Display = () => {
+const Display = ({ip, location, timezone, isp}) => {
     return ( 
     <div className="display-wrapper">
-        <div className="ip-address">IP ADDRESS<br/><br/><span>192.168.0.1</span></div>
-        <div className="location">LOCATION<br/><br/><span>Żywiec</span></div>
-        <div className="timezone">TIMEZONE<br/><br/><span>CEST-4:20</span></div>
-        <div className="isp">ISP<br/><br/><span>Monke</span></div>
+        <div className="ip-address">IP ADDRESS<br/><br/><span>{ip}</span></div>
+        <div className="location">LOCATION<br/><br/><span>{location}</span></div>
+        <div className="timezone">TIMEZONE<br/><br/><span>{timezone}</span></div>
+        <div className="isp">ISP<br/><br/><span>{isp}</span></div>
     </div>
      );
 }
